20cdfa3aede554488c1b2b8c1a2937968f690ce7
Hcyaaaby <135298382+Hcyaaaby@users.noreply.github.com>
2023-06-09 05:16:25 +0800

    Add files via upload

--- type.js
+++ type.js
@@ -1,14 +1,6 @@
-const menuItems = document.querySelectorAll('.menu-item');
-
-menuItems.forEach((menuItem) => {
-  menuItem.addEventListener('mouseleave', () => {
-    const subMenu = menuItem.querySelector('.sub-menu');
-    subMenu.style.display = 'none';
-  });
-});
-
 window.addEventListener('scroll',() => {
     const heroTitle = document.querySelector('.hero-title');
     heroTitle.style.transform = 
     `translateY(${window.scrollY * 0.5}px)`;
 });
+
